2b6bab80cd85871b61072a0096b5bcd82ebce1b9
arnav <aranja776@pccsk12.com>
2022-03-27 10:56:53 -0400

    HR1dArrayP2- play a game on a array
    HRMD5-
    encrypt a string in MD5
    
    HRSHA256-
    encrypt a string in SHA 256

---
+++ src/com/HackerRank/Examples/HRMD5.java
@@ -0,0 +1,24 @@
+package com.HackerRank.Examples;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class HRMD5 {
+
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        Scanner scan = new Scanner(System.in);
+
+        String str = scan.next();
+        scan.close();
+
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(str.getBytes());
+        byte[] digest = md.digest();
+
+        for( byte b : digest) {
+            System.out.format("%02x", b);
+        }
+    }
+}
